e65799b70d7124cb551b076118938cfe7e48050f
anhtv <anhtvgch190350@fpt.edu.vn>
2022-04-27 15:35:08 +0700

    first

---
+++ js/data.js
@@ -0,0 +1,11 @@
+const employee = [] = [
+    { id: 1, name: 'Mr. Nice', email: 'India', age: 18, salary: 2000, url: 'https://image.thanhnien.vn/w660/Uploaded/2022/ygtmjz/2021_02_27/superman_rkzd.jpg' },
+    { id: 2, name: 'Narco', email: 'USA', age: 19, salary: 3000, url: 'https://kenh14cdn.com/thumb_w/660/203336854389633024/2021/10/24/photo-1-16350703781592041092293.jpg' },
+    { id: 3, name: 'Bombasto', email: 'UK', age: 20, salary: 4000, url: 'https://gamek.mediacdn.vn/133514250583805952/2020/11/30/3-16067379530051162834010.jpg' },
+    { id: 4, name: 'Celeritas', email: 'Canada', age: 22, salary: 5000, url: 'https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fbatman-arkham-knight%2FEGS_WB_Batman_Arkham_Knight_G1_1920x1080_19_0911-1920x1080-1d69e15f00cb5ab57249f208f1f8f45d52cbbc59.jpg' },
+    { id: 5, name: 'Magneta', email: 'Russia', age: 23, salary: 6000, url: 'https://media.newyorker.com/photos/593581e785bd115baccba6d2/master/pass/Lane-Ten-Things-about-Wonder-Woman.jpg' },
+    { id: 6, name: 'RubberMan', email: 'China', age: 99, salary: 7000, url: 'https://wallpaperaccess.com/full/312463.jpg' },
+
+];
+
+console.log(employee);
